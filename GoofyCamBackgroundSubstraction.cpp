c5a2cd5f8705476353aa88a0407b67f3be16a64b
XXX <XXX@XXX.local>
2014-06-27 16:00:48 +0200

    Aggiunta la classe per il background substraction usando la webcam

---
+++ GoofyCamBackgroundSubstraction.cpp
@@ -0,0 +1,68 @@
+//
+//  GoofyCamBackgroundSubstraction.cpp
+//  LaLinea2
+//
+//  Created by XXX on 27/06/14.
+//
+//
+
+#include "GoofyCamBackgroundSubstraction.h"
+
+
+GoofyCamBackgroundSubstraction::GoofyCamBackgroundSubstraction()
+{
+  
+}
+
+void GoofyCamBackgroundSubstraction::setup(int width, int height, bool useOpticalFlow)
+{
+  cam.setDeviceID(0);
+  cam.setDesiredFrameRate(60);
+  cam.initGrabber(width,height);
+  
+  this->width = width;
+  this->height = height;
+  this->activeOpticalFlow = useOpticalFlow;
+  initFrameBuffer(width, height, backgroundFbo);
+  initFrameBuffer(width, height, substractionFbo);
+  
+  substractionShader.load("backgroundSubstraciton.vert","backgroundSubstraciton.frag");
+}
+
+void GoofyCamBackgroundSubstraction::update()
+{
+  cam.update();
+  if (cam.isFrameNew())
+  {
+    substractionShader.load("backgroundSubstraciton.vert","backgroundSubstraciton.frag");
+    substractionFbo.begin();
+    substractionShader.begin();
+    substractionShader.setUniformTexture("background", backgroundFbo.getTextureReference(), 0);
+    substractionShader.setUniformTexture("original", cam.getTextureReference(), 1);
+    backgroundFbo.draw(0,0);
+    substractionShader.end();
+    substractionFbo.end();
+  }
+}
+
+void GoofyCamBackgroundSubstraction::draw()
+{
+  ofSetColor(255);
+  cam.draw(0,0,width, height);
+  substractionFbo.draw(0,height,width,height);
+}
+
+void GoofyCamBackgroundSubstraction::setBackground()
+{
+  backgroundFbo.begin();
+  cam.draw(0,0, width, height);
+  backgroundFbo.end();
+}
+
+void GoofyCamBackgroundSubstraction::initFrameBuffer(int width, int height, ofFbo& fb)
+{
+  fb.allocate(width, height, GL_RGBA32F);
+  fb.begin();
+  ofClear(0, 0, 0 ,255);
+  fb.end();
+}
---
+++ GoofyCamBackgroundSubstraction.h
@@ -0,0 +1,32 @@
+//
+//  GoofyCamBackgroundSubstraction.h
+//  LaLinea2
+//
+//  Created by XXX on 27/06/14.
+//
+//
+
+#ifndef __LaLinea2__GoofyCamBackgroundSubstraction__
+#define __LaLinea2__GoofyCamBackgroundSubstraction__
+
+#include "ofMain.h"
+
+class GoofyCamBackgroundSubstraction
+{
+public:
+                    GoofyCamBackgroundSubstraction();
+  void              setup(int width, int height, bool useOpticalFlow = false);
+  void              initFrameBuffer(int width, int height, ofFbo& fb);
+  void              setBackground();
+  void              update();
+  void              draw();
+  int               width;
+  int               height;
+  ofFbo             backgroundFbo;
+  ofFbo             substractionFbo;
+  ofShader          substractionShader;
+  ofParameter<bool> activeOpticalFlow;
+  ofVideoGrabber    cam;
+};
+
+#endif /* defined(__LaLinea2__GoofyCamBackgroundSubstraction__) */
